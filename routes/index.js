d3a9390deabd07e7a6a5d73663b4664dee3d2ec4
jeongraeJR <hwal92@naver.com>
2020-06-14 15:24:36 +0900

    login 창 변경

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    if (!req.user) {
+        res.render('login');
+    } else {
+        res.render('index');
+    }
 });
 
 router.get('/login', function (req, res, next) {
-  res.render('login', { title: 'Login' });
+    res.render('login', { title: 'Login' });
 });
 
 router.get('/signup', function (req, res, next) {
-  res.render('signup', { title: 'Sign Up' });
+    res.render('signup', { title: 'Sign Up' });
 });
 
 router.get('/category', function (req, res, next) {
-  res.render('category', { title: 'Category' });
+    res.render('category', { title: 'Category' });
 });
 
 router.get('/postForm', function (req, res, next) {
-  res.render('postForm', { title: 'Category' });
+    res.render('postForm', { title: 'Category' });
 });
 
 module.exports = router;
